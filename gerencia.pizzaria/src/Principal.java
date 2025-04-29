c94ca6a534033938a011a42d2ada68856483d8a8
Edgard Lamounier <elamounier@gmail.com>
2025-04-29 14:39:23 -0300

    Add files via upload

---
+++ gerencia.pizzaria/src/Principal.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 102069076
+ */
+public class Principal {
+    public static void main(String [] args){
+        new Login().setVisible(true);
+    }
+}
